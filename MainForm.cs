27db65230d41543c340cd4fd5aa0110a0f982abd
kuznecivGit <kuzneciv@gmail.com>
2025-02-24 05:28:45 +1000

    update MainForm

---
+++ MainForm.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using ZXing;
+using System.Media;
+using System.Reflection;
+using System.IO;
+namespace QRScannerWinForm
+{
+    public partial class MainForm : Form
+    {
+        private bool a = false;
+        private bool b = false;
+        public MainForm()
+        {
+            InitializeComponent();
+            this.MaximizeBox = false;
+        }
+
+        private void buttonLoad2_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void buttonQR2_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void buttonLoad2_Click_1(object sender, EventArgs e)
+        {
+
+        }
+        private void buttonQR2_Click_1(object sender, EventArgs e)
+        {
+            
+        }
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void pictureBox5_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+
+        }
+        private void pictureBox6_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void pictureBox7_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void linkLabel5_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+
+        }
+        private void linkLabel2_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+
+        }
+        private void linkLabel4_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+
+        }
+        private void linkLabel3_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+           
+
+        }
+    }
+}
